31113f237dc3c44fc24c5d8a138c61e09eb80808
ANKUSH SINGH <ankushsingh_se21a13_54@dtu.ac.in>
2023-07-03 01:37:03 +0530

    Update authPassport.js

--- route/authPassport.js
+++ route/authPassport.js
@@ -8,7 +8,7 @@ passport.use(
         {
             clientID: process.env.CLIENT_ID,
             clientSecret: process.env.CLIENT_SECRET,
-            callbackURL: "https://twitter-backend-flame.vercel.app/auth/google",
+            callbackURL: "/auth/google",
         },
         function (accessToken, refreshToken, profile, cb) {
           
