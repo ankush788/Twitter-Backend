d56619c4ec9dd6b5a79f77f0113feb955c1a0637
ANKUSH SINGH <ankushsingh_se21a13_54@dtu.ac.in>
2023-07-03 17:41:06 +0530

    Update authenication.js

--- middleware/authenication.js
+++ middleware/authenication.js
@@ -1,22 +1,22 @@
 const jwt = require('jsonwebtoken');
 
+
 const authenticate = (req, res, next) => {
-    const jwtToken = req.cookies.token; // Retrieve JWT token from the cookie
-    const googleToken = req.cookies['connect.sid']; // Retrieve Google authentication token from the cookie
-   console.log(googleToken);
-    if (jwtToken) {
-        try {
-            const decoded = jwt.verify(jwtToken, process.env.SECRET);
-            req.user = decoded;
+    const token = req.cookies.token; // Retrieve token from the cookie
+    if (!token) {
+        if (req.isAuthenticated()) {
             return next();
-        } catch (error) {
-            return res.status(401).json({ message: 'Invalid JWT token' });
+        } else {
+            return res.status(401).json({ message: 'Missing token' });
         }
-    } else if (googleToken) {
-        // Authenticate using session-based authentication with passport
-        passport.authenticate('google', { session: true })(req, res, next);
-    } else {
-        return res.status(401).json({ message: 'Missing token' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET);
+        req.user = decoded;
+        return next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Invalid token' });
     }
 };
 
