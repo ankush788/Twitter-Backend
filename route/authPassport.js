2666e68381ebbe3cfd9e75d642b4434603667005
ANKUSH SINGH <ankushsingh_se21a13_54@dtu.ac.in>
2023-07-04 01:01:29 +0530

    Update authPassport.js

--- route/authPassport.js
+++ route/authPassport.js
@@ -1,7 +1,8 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require('../models/user');
 const passport = require("passport");
-
+const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser');
 
 passport.use(
     new GoogleStrategy(
@@ -12,14 +13,16 @@ passport.use(
         },
         function (accessToken, refreshToken, profile, cb) {
           
+            const token = jwt.sign({ userId: profile.id, name: profile.displayName, email: profile.emails[0].value, photoLink: profile.photos[0].value}, process.env.SECRET, {
+                expiresIn: "2h",
+            });
+
             User.findOrCreate({ googleId: profile.id, name: profile.displayName, email: profile.emails[0].value, photoLink: profile.photos[0].value }, function (err, user) {
-                return cb(err, user);
+                return cb(err, {user, token});
             });
         }
     )
 );
-
-
 
 passport.serializeUser((user, done) => {
     done(null, user);
